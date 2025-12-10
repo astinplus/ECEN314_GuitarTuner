97a291e2c6cf84a45f62a6c28fb6d50043996f37
Austin Porter <austinlporter04@gmail.com>
2025-12-10 13:04:22 -0600

    no longer gets confused between Low E, A, and High E strings

--- venv/main.py
+++ venv/main.py
@@ -9,6 +9,11 @@ RATE = 96000#44100  # Sample rate (Hz)
 RECORD_SECONDS = 5  # Duration of recording
 STANDARD_TUNING = {82: "Low E", 110: "A", 147: "D", 196: "G", 247: "B", 330: "High E"}
 
+PLOTTING = False
+ERROR_THRESHOLD = 0.1
+N_MAX_VALUES = 3
+MIN_MAG_LIMIT = 200000
+
 def mag_response_X(f, data):
     result = 0
     for i in range(CHUNK):
@@ -19,8 +24,13 @@ def find_closest_string(data, tuning=STANDARD_TUNING):
     errors = dict.fromkeys(tuning.keys())
     for k in tuning.keys():
         errors[k] = sum([abs(float(d)/k - round(d/k)) for d in data])
+        # print("Integer Multiples of "+STANDARD_TUNING[k]+" string: "+str([round(d/k) for d in data]))
+        if(max([round(d/k) for d in data]) > 6):
+            errors[k] = len(data)
     # print(errors)
+
     most_likely = min(errors, key=errors.get)
+    # print("High E Error: "+str(errors[330]))
     return most_likely, errors[most_likely]/len(data)
 # set up pyaudio
 p = pyaudio.PyAudio()
@@ -30,17 +40,13 @@ stream = p.open(format=FORMAT,
                 rate=RATE,
                 input=True,
                 frames_per_buffer=CHUNK)
-
-SIZE = int(CHUNK/2+1)
-fig, ax = plt.subplots()
-# x = np.arange(0, SIZE, 1)  # X-axis for waveform
 x = np.fft.rfftfreq(CHUNK, 1.0/RATE)
-line, = ax.plot(x, np.random.rand(SIZE), '-')  # Initial empty plot
-# ax.set_ylim(-2 ** 15, 2 ** 15)  # Adjust based on your audio format (e.g., int16)
-ax.set_ylim(0, 500000)
-# ax.set_xlim(0, max(x))
-ax.set_xlim(0,800)
-plt.show(block=False)  # Non-blocking show
+if PLOTTING:
+    fig, ax = plt.subplots()
+    line, = ax.plot(x, np.random.rand(int(CHUNK/2+1)), '-')  # Initial empty plot
+    ax.set_ylim(0, 500000)
+    ax.set_xlim(0,1200)
+    plt.show(block=False)  # Non-blocking show
 
 # fig, ax = plt.subplots()
 # frequency_values = np.arange(0, 400, 1)
@@ -54,38 +60,45 @@ plt.show(block=False)  # Non-blocking show
 #     max_indices = np.argpartition(data, -n)[-n:]
 #     return freqs[max_indices], data[max_indices]
 
+if PLOTTING:
+    try:
+        while True:
+            data = stream.read(CHUNK)
+            # Convert byte data to numpy array
+            audio_data = np.frombuffer(data, dtype=np.int16)
 
-try:
-    while True:
-        data = stream.read(CHUNK)
-        # Convert byte data to numpy array
-        audio_data = np.frombuffer(data, dtype=np.int16)
+            fft_data = np.abs(np.fft.rfft(audio_data))
+            max_indices = np.argpartition(fft_data, -N_MAX_VALUES)[-N_MAX_VALUES:]
+            # print(x[max_indices])
+            c, e = find_closest_string(x[max_indices])
+            if e < 0.1 and sum(fft_data[max_indices]) > MIN_MAG_LIMIT and 0 not in x[max_indices] and min(x[max_indices] > 100):
+                print("This is the "+STANDARD_TUNING[c]+" string with error "+str(e))
+                # print("Maximum Frequencies: "+str(x[max_indices]))
 
-        fft_data = np.abs(np.fft.rfft(audio_data))
-        # print(fft_data)
-        # frequency_data = [None] * len(frequency_values)
-        # for i in range(len(frequency_values)):
-        #     frequency_data[i] = mag_response_X(frequency_values[i], audio_data)
-        #
-        # print(frequency_data)
-        # print(len(x))
-        # print(len(fft_data))
-        # print(x[np.argmax(fft_data)])
-        max_indices = np.argpartition(fft_data, -5)[-5:]
-        # mf, md = get_max_freqs(3, fft_data, x)
-        # print(x[max_indices], "max = "+str(x[np.argmax(fft_data)]))
-        c, e = find_closest_string(x[max_indices])
-        if e < 0.1 and 0 not in x[max_indices]:
-            print("This is the "+STANDARD_TUNING[c]+" string with confidence "+str(e))
-            # print(c,e)
-        # Update the plot
-        line.set_ydata(fft_data)
-        # line.set_ydata(frequency_data)
-        fig.canvas.draw()
-        fig.canvas.flush_events()
+            # Update the plot
+            line.set_ydata(fft_data)
+            fig.canvas.draw()
+            fig.canvas.flush_events()
 
-except KeyboardInterrupt:
-    pass
+    except KeyboardInterrupt:
+        pass
+else:
+    try:
+        while True:
+            data = stream.read(CHUNK)
+            # Convert byte data to numpy array
+            audio_data = np.frombuffer(data, dtype=np.int16)
+
+            fft_data = np.abs(np.fft.rfft(audio_data))
+            max_indices = np.argpartition(fft_data, -N_MAX_VALUES)[-N_MAX_VALUES:]
+            c, e = find_closest_string(x[max_indices])
+            if e < 0.1 and sum(fft_data[max_indices]) > MIN_MAG_LIMIT and 0 not in x[max_indices] and min(x[max_indices] > 100):
+                print("This is the " + STANDARD_TUNING[c] + " string with error " + str(e))
+                # print("Maximum Frequencies: "+str(x[max_indices]))
+
+    except KeyboardInterrupt:
+        pass
+
 
 stream.stop_stream()
 stream.close()
